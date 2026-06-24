bb8ac70f178990ec9d5f49458e9c7f94d6838bf8
Daudi <davidonya2@gmail.com>
2026-06-24 16:43:21 +0300

    Update src/app/specialist/notifications/

---
+++ src/app/specialist/notifications/page.tsx
@@ -0,0 +1,5 @@
+import { NotificationsPage } from "@/components/ui/NotificationsPage";
+
+export default function SpecialistNotificationsRoute() {
+  return <NotificationsPage role="specialist" />;
+}
